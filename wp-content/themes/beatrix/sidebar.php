5d1015184b18f106b7785a0ab2686078fc53f209
JJ <jj@jjkeith.net>
2018-03-11 15:43:02 -0700

    Began custom theme

---
+++ wp-content/themes/beatrix/sidebar.php
@@ -0,0 +1,21 @@
+<div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+          <div class="sidebar-module sidebar-module-inset">
+            <h4>About</h4>
+            <p><?php the_author_meta( 'description' ) ?></p>
+          </div>
+          <div class="sidebar-module">
+            <h4>Archives</h4>
+            <ol class="list-unstyled">
+              <?php wp_get_archives( 'type=monthly' ); ?>
+            </ol>
+          </div>
+          <div class="sidebar-module">
+            <h4>Elsewhere</h4>
+            <ol class="list-unstyled">
+              <li><a href="http://www.hiyajj.com" target="_blank">Portfolio</a></li>
+              <li><a href="https://www.linkedin.com/in/jjkeith/" target="_blank">LinkedIn</li>
+              <li><a href="http://www.github.com/jjkeith" target="_blank">GitHub</a></li>
+              <li><a href="http://www.twitter.com/jj_keith" target="_blank">Twitter</a></li>
+            </ol>
+          </div>
+        </div><!-- /.blog-sidebar -->
